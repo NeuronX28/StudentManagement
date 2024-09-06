5242bdc241c989b319c8294627f5d7db862effa1
vish <vishw2810@gmail.com>
2024-09-06 22:10:02 +0530

    sign up page done

--- app/src/main/java/com/example/studentmanagement/MainActivity.java
+++ app/src/main/java/com/example/studentmanagement/MainActivity.java
@@ -14,7 +14,7 @@ public class MainActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_login_page);
+        setContentView(R.layout.activity_sign_up);
 
 
     }
