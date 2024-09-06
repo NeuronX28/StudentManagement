549cd3404fb76cc1c0f05c75ee9809d753db23f4
vish <vishw2810@gmail.com>
2024-09-06 19:49:41 +0530

    ok

--- app/src/main/java/com/example/studentmanagement/MainActivity.java
+++ app/src/main/java/com/example/studentmanagement/MainActivity.java
@@ -1,6 +1,7 @@
 package com.example.studentmanagement;
 
 import android.os.Bundle;
+import android.widget.Toast;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -15,6 +16,7 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
         setContentView(R.layout.activity_main);
+        //test
 
     }
 }
